e30e51e16a8bf0c15f4d38cf5a2e33b912fbcde0
Sobana Qadus <sobanaq@hotmail.com>
2022-12-16 09:48:05 +0000

    JS added

--- cyberpetscript.js
+++ cyberpetscript.js
@@ -1,15 +1,33 @@
-class Pet [Dog,Rabbit,Cat,Parrot]
-constructor Pet(){
-    this.petName = petName
+class pet {
+  constructor() {
+    this.petName = petName;
+    this.health = 100;
+    this.hungry = 100;
+  }
+
+  //Method
+  feed() {
+    this.hungry <= 5;
+    this.health <= 10;
+  }
 }
 
+document.getElementById("submitBtn").addEventListener("click", () => {
+  let chosenAnimal = null;
 
-class Rabbit {
-    this.petName = petName
+  document.querySelectorAll(".choice").forEach((element) => {
+    if (element.checked) {
+      chosenAnimal = element.value;
+    }
+  });
+});
+
+if (chosenAnimal === "cat") {
+  console.log("THE CAT HEALTH STUFF ETC");
+} else if (chosenAnimal === "Dog") {
+  console.log("THE DOG HEALTH STUFF ETC");
+} else if (chosenAnimal === "Rabbit") {
+  console.log("THE Rabbit HEALTH STUFF ETC");
+} else if (chosenAnimal === "Parrot") {
+  console.log("THE PARROT HEALTH STUFF ETC");
 }
-
-class Cat {
-
-}
-
-class Parrot
